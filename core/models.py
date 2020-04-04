0a51331b4cc94a54829aef4c2905246607228ff1
Marcelo Souza <magosou@gmail.com>
2020-04-04 11:45:08 -0300

    adicionadas funções de alteração e deleção de eventos

--- core/models.py
+++ core/models.py
@@ -8,6 +8,7 @@ class Evento(models.Model):
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
+    local_evento = models.CharField(max_length=100, blank=True, null=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE) # se o usuário for excluido os eventos dele também saõ
 
     class Meta:
@@ -18,6 +19,9 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%Mhrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
 
 
 
